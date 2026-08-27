21294854cb78227f41286e29fbbb97092ce36462
iamerenyeager7359-cloud <iamerenyeager7359@gmail.com>
2026-08-27 15:21:02 +0530

    Change default MUSIC_BOT_NAME to 'Sneha music'

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "5287826114"))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Sofiya music")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Sneha music")
 
 # Your User ID.
 OWNER_ID = list(
